f1d08fb0962309680d2cd71aca5a3cb96859bcef
confor <confor@users.noreply.github.com>
2023-01-22 07:04:00 -0300

    working crawler + storage

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+import re
+import os
 from functions import get, extract_links
 
 import ignition  # type: ignore
 ignition.set_default_timeout(4)
 
-ROOT = 'gemini://vodoraslo.xyz'
+ROOT = 'gemini://aperalesf.flounder.online/'
+TARGET = 'aperalesf'
 
 CRAWLER = {}
+
+
+def safe_name(string: str) -> str:
+    pattern = re.compile('[^a-zA-Z0-9_]')
+    return re.sub(pattern, '_', string)
+
+
+def store(name, body):
+    file = open(TARGET + '/' + name, 'w')
+    file.write(body)
+    file.flush()
+    file.close()
 
 
 def fetch(url, previous_url):
@@ -16,9 +31,12 @@ def fetch(url, previous_url):
         CRAWLER[url] = True
         return
 
-    print(f'Downloading {url}')
+    print(f'Downloading {url} as {safe_name(url)}')
     CRAWLER[url] = True
     res, body = get(url)
+
+    file_data = '\r\n'.join(res.headers) + '\r\n\r\n' + body
+    store(safe_name(url), file_data)
 
     if res.client_status == 3:
         print(f'Found redirect: {res.data}')
@@ -35,4 +53,8 @@ def fetch(url, previous_url):
     return
 
 
+if not os.path.isdir(TARGET):
+    os.mkdir(TARGET)
+
+print(f'Crawling {ROOT} and storing in {TARGET}...')
 fetch('/', ROOT)
