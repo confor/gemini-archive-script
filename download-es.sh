11c7c0e87015b8699a8bf98ae219b1184cac8034
confor <confor@users.noreply.github.com>
2023-01-25 16:55:54 -0300

    add new capsules, remove dead ones

--- download-es.sh
+++ download-es.sh
@@ -8,25 +8,29 @@ capsules=(
 	"gemini://gemini.astropirados.space" "resetreboot"
 	"gemini://gemini.elbinario.net" "elbinario"
 	"gemini://reisub.nsupdate.info" "reisub-hosting-gemini-free-50m"
-	"gemini://c3po.aljadra.xyz" "c3po"
+	#"gemini://c3po.aljadra.xyz" "c3po"  # connection refused
 	"gemini://compudanzas.net" "compudanzas"
 	"gemini://alex.corcoles.net" "alex.corcoles.net"
-	#"gemini://costas.dev" "ariel_costas"
-	"gemini://bucareli.online" "bucareli"
+	#"gemini://costas.dev" "ariel_costas"  # offline
+	#"gemini://bucareli.online" "bucareli"  # offline
 	"gemini://sitio.bitsandlinux.com" "davidochobits"
-	#"gemini://elmau.net" "elmau.net"
+	#"gemini://elmau.net" "elmau.net"  # offline
 	"gemini://gemlog.gamifi.cat" "gamifi.cat"
 	"gemini://hispagatos.org" "hispagatos"
-	"gemini://ivanruvalcaba.cf" "ivanruvalcaba.cf"
+	#"gemini://ivanruvalcaba.cf" "ivanruvalcaba.cf"  # offline
 	"gemini://gem.juancastro.xyz" "juancastro.xyz"
 	"gemini://srlobo.gorritodeplata.xyz" "srlobo"
-	"gemini://michan.noho.st" "roboron-noho.st"
-	"gemini://gemini.kosmonautik.net" "kosmonautik"
+	#"gemini://michan.noho.st" "roboron-noho.st"  # offline
+	#"gemini://gemini.kosmonautik.net" "kosmonautik"  # offline
 	"gemini://gemini.solobsd.org" "thedarkmuon"
 	"gemini://jorgesanz.net" "jorgesanz.net"
 	"gemini://michan.es" "roboron-michan.es"
 	"gemini://frankenwolke.com" "frankenwolke.com"
 	"gemini://gmi.osiux.com" "osiux"
+	"gemini://feriados.cl" "feriados.cl"
+	"gemini://gemini.andros.dev" "andros.dev"
+	"gemini://gmi.andremor.dev" "andremor"
+	"gemini://tecnologiaensimple.cl" "tecnologiaensimple"
 
 	"gemini://archipielago.uno" "archipielago.uno"
 	"gemini://aves.archipielago.uno" "aves.archipielago.uno"
@@ -36,7 +40,7 @@ capsules=(
 	"gemini://hache.archipielago.uno" "hache.archipielago.uno"
 	"gemini://lind.archipielago.uno" "lind.archipielago.uno"
 	"gemini://ness.archipielago.uno" "ness.archipielago.uno"
-	"gemini://sejo.archipielago.uno" "sejo.archipielago.uno"
+	#"gemini://sejo.archipielago.uno" "sejo.archipielago.uno"  # not found
 	"gemini://tralfanum.archipielago.uno" "tralfanum.archipielago.uno"
 	"gemini://wiki.archipielago.uno" "wiki.archipielago.uno"
 
@@ -51,7 +55,8 @@ capsules=(
 	"gemini://my32.flounder.online" "my32.flounder.online"
 	"gemini://pandora.flounder.online" "pandora.flounder.online"
 	"gemini://soyricky.flounder.online" "soyricky.flounder.online"
-	"gemini://xrasl.flounder.online" "xrasl.flounder.online"
+	#"gemini://xrasl.flounder.online" "xrasl.flounder.online"  # not found
+	"gemini://andalucia.flounder.online" "andalucia.flounder.online"
 )
 
 target="archive"
